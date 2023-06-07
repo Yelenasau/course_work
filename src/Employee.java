8895eae7d1f7822896fd522e929368f8a303ee6f
Yelenasau <elenasauschkina@mail.ru>
2023-06-07 21:25:44 +0400

    второй печальный комит

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,49 @@
 public class Employee {
+
+    private static int idGenerator = 0;
+    private final int id;
+    private final String fio;
+    private int department;
+    private int salary;
+
+
+    public Employee(String fio, int department, int salary) {
+        this.fio = fio;
+        this.department = department;
+        this.salary = salary;
+        this.id = ++idGenerator;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getFio() {
+        return fio;
+    }
+    public int getDepartment() {
+            return department;
+        }
+
+    public int getSalary() {
+        return salary;
+    }
+    public void setDepartment (int department) {
+        if (department > 0 && department <= 5 ) {
+            this.department = department;
+        }
+    }
+    public void setSalary (int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fio='" + fio + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,82 @@ public class Main {
     //    3. Найти сотрудника с минимальной зарплатой.
     //    4. Найти сотрудника с максимальной зарплатой.
     //    5. Подсчитать среднее значение зарплат (можно использовать для этого метод из пункта b).
-    //    6. Получить Ф. И. О. всех сотрудников (вывести в консоль).
+    //    6. Получить Ф. И. О. всех сотрудников (вывести в консоль)
+    private static final Employee[] employees = new Employee[10];
 
     public static void main(String[] args) {
+        employees[0] = new Employee("Иванов Иван Иванович", 1, 61600);
+        employees[1] = new Employee("Петров Петр Петрович", 1, 67500);
+        employees[2] = new Employee("Сидоров Сидор Сидорович", 2, 62500);
+        employees[3] = new Employee("Федоров Федор Федорович", 2, 66900);
+        employees[4] = new Employee("Морозов Михаил Михайлович", 3, 65500);
+        employees[5] = new Employee("Федоров Федор Федорович", 3, 55500);
+        employees[6] = new Employee("Игорев Игорь Игоревич", 4, 59500);
+        employees[7] = new Employee("Борисов Борис Борисович", 4, 75500);
+        employees[8] = new Employee("Володин Владимир Владимирович", 5, 85500);
+        employees[9] = new Employee("Миронов Мирон Миронович", 5, 45500);
+        printAll();
+        System.out.println("Сумма затрат в месяц : " + sum());
+        findMinSalary();
+        findMaxSalary();
+        System.out.println("Cредняя зп : " + findAverageSalary());
+        printFio();
+    }
+
+    static void printAll() {
+
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+
+    static int sum() {
+        int sum = 0;
+        for (Employee employee : employees) {
+            sum = sum + employee.getSalary();
+        }
+        return sum;
+    }
+
+    static void findMinSalary() {
+        int min = Integer.MAX_VALUE;
+        int index = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() < min) {
+                min = employees[i].getSalary();
+                index = i;
+            }
+        }
+        System.out.println("Cотрудник с минимальной зп " + employees[index]);
+//
+    }
+
+    static void findMaxSalary() {
+        int max = Integer.MIN_VALUE;
+        int index = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() > max) {
+                max = employees[i].getSalary();
+                index = i;
+            }
+        }
+        System.out.println("Cотрудник с максимальной зп " + employees[index]);
+    }
+
+    static int findAverageSalary() {
+        int sum = 0;
+        int averageValue = 0;
+        for (Employee employee : employees) {
+            sum = sum + employee.getSalary();
+            averageValue = sum / employees.length - 1;
+        }
+        return averageValue;
+    }
+
+    static void printFio() {
+        for (Employee employee : employees) {
+            System.out.println(employee.getFio());
+        }
+
     }
 }
